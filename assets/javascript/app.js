b7ef302504202cf9ac2da87c81ec21b53f4c3522
BrittGall <crlnfllr@gmail.com>
2019-08-19 23:01:20 -0500

    start btn hides on click, array of questions made

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,56 @@
 $(document).ready(function() {
- //variables
+  //variables
   let number = 30;
-let numberTwo=5;
+  let numberTwo = 5;
   let timer;
-  const questionArray = [];
-// questions and answers
-  const q1 = {
-    q: "What was the first 3D animated TV show?",
-    a: "Cubix",
-    b: "ReBoot",
-    c: "Jimmy Neutron",
-    d: "Toy Story"
-  };
-  const q2 = { q: "What was the first animated feature movie?",
-  a: "Snow White", 
-  b: "Adventures of Prince Achmed", 
-  c: "Tale of the White Serpent", 
-  d:"Fantasmagorie"
-},
-  const q3 = { q: "Which was Walt Disney's favorite movie that he produced?",
-  a: "Snow White", 
-  b: "101 Dalmations", 
-  c:"Cinderella", 
-  d: "Bambi"
-};
-  const q4 = { q: "Which is NOT a Don Bluth movie?",
-  a: "Anastasia",
-  b: "An American Tale",
-  c: "Secret of Nimb",
-  d: "Quest for Camelot" 
-};
-  const q5 = { q: "Who was Walt Disney's personal favorite concept artist?",
-a: "Glene Keane",
-b: "Mary Blair",
-c: "Floyd Norman",
-d: "Don Lusk" };
 
-//on button click, timer starts
-  $("#button").on("click", run);
+  // questions and answers
+  const questionArray = [
+    {
+      q: "What was the first 3D animated TV show?",
+      a: "Cubix",
+      b: "ReBoot",
+      c: "Jimmy Neutron",
+      d: "Toy Story"
+    },
+    {
+      q: "What was the first animated feature movie?",
+      a: "Snow White",
+      b: "Adventures of Prince Achmed",
+      c: "Tale of the White Serpent",
+      d: "Fantasmagorie"
+    },
+    {
+      q: "Which was Walt Disney's favorite movie that he produced?",
+      a: "Snow White",
+      b: "101 Dalmations",
+      c: "Cinderella",
+      d: "Bambi"
+    },
+    {
+      q: "Which is NOT a Don Bluth movie?",
+      a: "Anastasia",
+      b: "An American Tale",
+      c: "Secret of Nimb",
+      d: "Quest for Camelot"
+    },
+    {
+      q: "Who was Walt Disney's personal favorite concept artist?",
+      a: "Glene Keane",
+      b: "Mary Blair",
+      c: "Floyd Norman",
+      d: "Don Lusk"
+    }
+  ];
 
-  // $("#button").hide();
-//shows starting number
+  //on button click, timer starts
+  $("#button").click(function() {
+    run();
+    $("#button").hide();
+  });
+  //shows starting number
   $("#timer").html("<h2>" + number + "</h2>");
-//starts 
+  //starts
   function run() {
     timer = setInterval(decrement, 1000);
   }
